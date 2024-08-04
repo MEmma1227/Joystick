b1f0abdd68d3c14cb1b8c07f51421853cfb277f4
MEmma1227 <106003218+MEmma1227@users.noreply.github.com>
2024-08-04 17:48:55 +0800

    Create mg996r_test_pigpio.py

---
+++ mg996r/mg996r_test_pigpio.py
@@ -0,0 +1,41 @@
+## 使用 pigpio package
+## 馬達從0°每次遞增+10°直到180°
+## mg996r 必須是 180 規格的
+
+import pigpio
+import time
+
+servo_pin = 18
+
+pwm = pigpio.pi()
+pwm.set_mode(servo_pin, pigpio.OUTPUT)
+pwm.set_PWM_frequency(servo_pin,50) #50Hz frequency
+
+def destroy():
+    pwm.set_PWM_dutycycle(servo_pin, 0)
+    pwm.set_PWM_frequency(servo_pin, 0)
+    pwm.stop()
+
+# input 0~180 degree (DO NOT exceed 180)
+def set_angle(angle):
+    # duty_cycle description
+    # 0 = stop
+    # 500 = 0 degree
+    # 1500 = 90 degree
+    # 2500 = 180 degree
+    duty = 500 + (float(angle) / 180.0) * 2000  # python2 需要用float寫, python3 則可直接除掉
+    pwm.set_servo_pulsewidth(servo_pin, duty)
+    print("angle = ", angle, " -> duty = ", duty)
+    time.sleep(0.5)
+
+def run():
+    for angle in range(0, 181, 10):
+        set_angle(angle)
+        time.sleep(1)
+
+if __name__ == "__main__":
+    try:
+        run()
+    finally:
+        destroy()
+
